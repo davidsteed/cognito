a398e4ca483217997eb62b4d1c4641021e028388
davidsteed <davidsteed1@gmail.com>
2025-02-26 14:41:02 +0000

    feat: added cognito

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,36 @@
+"use client";
 import AcmeLogo from "@/app/ui/acme-logo";
-import LoginForm from "@/app/ui/login-form";
 import { Suspense } from "react";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
 
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.AWS_COGNITO_POOL_ID!,
+      userPoolClientId: process.env.AWS_COGNITO_APP_CLIENT_ID!,
+    },
+  },
+});
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+      <div className="relative mx-auto flex w-full max-w-[512px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
             <AcmeLogo />
           </div>
         </div>
         <Suspense>
-          <LoginForm />
+          <Authenticator hideSignUp>
+            {({ signOut, user }) => (
+              <main>
+                <h1>Hello {user?.username}</h1>
+                <button onClick={signOut}>Sign out</button>
+              </main>
+            )}
+          </Authenticator>
         </Suspense>
       </div>
     </main>
